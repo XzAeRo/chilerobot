ef44abdeebe5df6b1d7fe33fbd9089bc2b1dd33b
Victor Gonzalez <victor.gonzalezro@gmail.com>
2016-10-13 12:11:24 -0300

    add first tests
    Signed-off-by: Victor Gonzalez <victor.gonzalezro@gmail.com>

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import praw
 import settings
-import os
+from scrappers import ElMostrador
+import os, pprint
+import markdown
 
-user_agent = ("User-Agent: python:cl.vgr.chilerobot:v1.0.0 (by /u/XzAeRosho) github.com/xzaero/chilerobot")
+def main():
+    user_agent = ("User-Agent: python:cl.vgr.chilerobot:v1.0.0 (by /u/XzAeRosho) github.com/xzaero/chilerobot")
 
-reddit = praw.Reddit(user_agent=user_agent,
-                     client_id=os.environ.get("CLIENT_ID"),
-                     client_secret=os.environ.get("CLIENT_SECRET"),
-                     username=os.environ.get("USERNAME"),
-                     password=os.environ.get("PASSWORD"))
+    reddit = praw.Reddit(
+        user_agent=user_agent,
+        client_id=os.environ.get("CLIENT_ID"),
+        client_secret=os.environ.get("CLIENT_SECRET"),
+        username=os.environ.get("USERNAME"),
+        password=os.environ.get("PASSWORD")
+    )
 
-for submission in reddit.subreddit('chile').hot(limit=10):
-    print(submission.title)
+    subreddit = reddit.subreddit('chilerobot_bootcamp')
+    #for submission in reddit.subreddit('chile').hot(limit=20):
+    #    process_submission(submission)
+    for submission in subreddit.stream.submissions():
+        process_submission(submission)
+
+def process_submission(submission):
+    supported_domains = ['elmostrador.cl']
+
+    if submission.domain in supported_domains:
+        print(settings.REPLY_TEMPLATE.format('Titulo','Cuerpo'))
+        submission.reply(settings.REPLY_TEMPLATE.format('Titulo','Cuerpo'))
+
+
+if __name__ == '__main__':
+    main()
--- settings.py
+++ settings.py
@@ -3,3 +3,10 @@ from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
+REPLY_TEMPLATE = """#{}
+
+{}
+
+**ChileRobot** | [Autor](/u/XzAeRosho) | [Código Fuente](https://github.com/xzaero/chilerobot)
+"""
